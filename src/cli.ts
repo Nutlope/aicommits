aaec3278465b083dbbec72b3fdd23d4d4ce23621
Guilherme Rigotti <ggrigotti@gmail.com>
2023-02-16 14:06:47 -0300

    Add ESLint configuration and packages for linting and formatting code.

--- src/cli.ts
+++ src/cli.ts
@@ -1,59 +1,60 @@
-import { execa } from 'execa';
+import { execa } from "execa";
+import { black, green, red, bgCyan } from "kolorist";
+import { intro, outro, spinner, confirm, isCancel } from "@clack/prompts";
 import {
-	black, green, red, bgCyan,
-} from 'kolorist';
-import {
-	intro, outro, spinner, confirm, isCancel,
-} from '@clack/prompts';
-import {
-	getConfig,
-	assertGitRepo,
-	getStagedDiff,
-	getDetectedMessage,
-	generateCommitMessage,
-} from './utils.js';
+  getConfig,
+  assertGitRepo,
+  getStagedDiff,
+  getDetectedMessage,
+  generateCommitMessage,
+} from "./utils.js";
 
 (async () => {
-	intro(bgCyan(black(' aicommits ')));
+  intro(bgCyan(black(" aicommits ")));
 
-	await assertGitRepo();
+  await assertGitRepo();
 
-	const detectingFiles = spinner();
-	detectingFiles.start('Detecting staged files');
-	const staged = await getStagedDiff();
+  const detectingFiles = spinner();
+  detectingFiles.start("Detecting staged files");
+  const staged = await getStagedDiff();
 
-	if (!staged) {
-		throw new Error('No staged changes found. Make sure to stage your changes with `git add`.');
-	}
+  if (!staged) {
+    throw new Error(
+      "No staged changes found. Make sure to stage your changes with `git add`."
+    );
+  }
 
-	detectingFiles.stop(`${getDetectedMessage(staged.files)}:\n${
-		staged.files.map(file => `     ${file}`).join('\n')
-	}`);
+  detectingFiles.stop(
+    `${getDetectedMessage(staged.files)}:\n${staged.files
+      .map((file) => `     ${file}`)
+      .join("\n")}`
+  );
 
-	const config = await getConfig();
-	const OPENAI_KEY = process.env.OPENAI_KEY ?? process.env.OPENAI_API_KEY ?? config.OPENAI_KEY;
-	if (!OPENAI_KEY) {
-		throw new Error('Please set your OpenAI API key in ~/.aicommits');
-	}
+  const config = await getConfig();
+  const OPENAI_KEY =
+    process.env.OPENAI_KEY ?? process.env.OPENAI_API_KEY ?? config.OPENAI_KEY;
+  if (!OPENAI_KEY) {
+    throw new Error("Please set your OpenAI API key in ~/.aicommits");
+  }
 
-	const s = spinner();
-	s.start('The AI is analyzing your changes');
-	const message = await generateCommitMessage(OPENAI_KEY, staged.diff);
-	s.stop('The commit message is ready for review');
+  const s = spinner();
+  s.start("The AI is analyzing your changes");
+  const message = await generateCommitMessage(OPENAI_KEY, staged.diff);
+  s.stop("The commit message is ready for review");
 
-	const confirmed = await confirm({
-		message: `Would you like to commit with this message:\n\n   ${message}\n`,
-	});
+  const confirmed = await confirm({
+    message: `Would you like to commit with this message:\n\n   ${message}\n`,
+  });
 
-	if (!confirmed || isCancel(confirmed)) {
-		outro('Commit cancelled');
-		return;
-	}
+  if (!confirmed || isCancel(confirmed)) {
+    outro("Commit cancelled");
+    return;
+  }
 
-	await execa('git', ['commit', '-m', message]);
+  await execa("git", ["commit", "-m", message]);
 
-	outro(`${green('✔')} Successfully committed!`);
+  outro(`${green("✔")} Successfully committed!`);
 })().catch((error) => {
-	outro(`${red('✖')} ${error.message}`);
-	process.exit(1);
+  outro(`${red("✖")} ${error.message}`);
+  process.exit(1);
 });
