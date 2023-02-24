474e356663e7028b0f89a16036a8d46fa4a2e583
Simon Liang <no.lhr0909@gmail.com>
2023-02-24 20:17:20 +0800

    Add dependency '@dqbd/tiktoken' and update OpenAI configuration to use 'text-davinci-003' model

--- src/utils/openai.ts
+++ src/utils/openai.ts
@@ -1,10 +1,14 @@
 import { Configuration, OpenAIApi } from 'openai';
+import { encoding_for_model as encodingForModel } from '@dqbd/tiktoken';
 
 const sanitizeMessage = (message: string) => message.trim().replace(/[\n\r]/g, '').replace(/(\w)\.$/, '$1');
 
 const deduplicateMessages = (array: string[]) => Array.from(new Set(array));
 
 const promptTemplate = 'Write an insightful but concise Git commit message in a complete sentence in present tense for the following diff without prefacing it with anything:';
+
+const MODEL = 'text-davinci-003';
+const encoder = encodingForModel(MODEL);
 
 export const generateCommitMessage = async (
 	apiKey: string,
@@ -14,14 +18,14 @@ export const generateCommitMessage = async (
 	const prompt = `${promptTemplate}\n${diff}`;
 
 	// Accounting for GPT-3's input req of 4k tokens (approx 8k chars)
-	if (prompt.length > 8000) {
+	if (encoder.encode(prompt).length > 4000) {
 		throw new Error('The diff is too large for the OpenAI API. Try reducing the number of staged changes, or write your own commit message.');
 	}
 
 	const openai = new OpenAIApi(new Configuration({ apiKey }));
 	try {
 		const completion = await openai.createCompletion({
-			model: 'text-davinci-003',
+			model: MODEL,
 			prompt,
 			temperature: 0.7,
 			top_p: 1,
