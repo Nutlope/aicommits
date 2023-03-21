9be263dfd65b76a371c3d17d5737705a167df863
Thijs Limmen <tlimmen@xpirit.com>
2023-03-21 19:48:33 +0100

    chore: 🚧 Update openai.ts file
    Update the getBasePrompt function in openai.ts file to remove the INSTRUCTIONS header and add the language of the commit. Update the getCommitMessageExtraContext function to add guidelines for the commit title and description. The commit title should be no longer than 50 characters and the commit description should be no longer than 72 characters. The commit message should be in the following format: <conventional commits type>(<optional scope of the change>): <gitmoji> <commit title>/n/n<commit description>. This commit includes changes to improve the prompt and formatting of the commit message.

--- src/utils/openai.ts
+++ src/utils/openai.ts
@@ -88,37 +88,44 @@ const createChatCompletion = async (
 const deduplicateMessages = (array: string[]) => Array.from(new Set(array));
 
 const getBasePrompt = (locale: string) => `
-INSTRUCTIONS:\nI want you to act as the author of a commit message in git.
+I want you to act as the author with language ${locale} of a commit message in git.
 I'll enter a git diff, and your job is to convert it into a useful commit message.
-Do not preface the commit with anything, use the present tense, return the full sentence.
-The commit message must be in the language: ${locale}.`;
+Do not preface the commit with anything, use the present tense, return the full sentence.`;
 
-const getCommitMessageFormatPrompt = (useConventionalCommits: boolean, useGitmoji: boolean) => {
-	const commitFormatParts = [];
+const getCommitMessageExtraContext = (locale: string) => `
+The <commit title> must be in the language: ${locale}.
+The <commit description> must be in the language: ${locale}.
+The <commit title> should be no longer than 50 characters.
+The <commit description> should be no longer than 72 characters.
+`;
+
+const getCommitTitleFormatPrompt = (useConventionalCommits: boolean, useGitmoji: boolean) => {
+	const commitTitleParts = [];
 
 	if (useConventionalCommits) {
-		commitFormatParts.push('<conventional commits type (<type in lowercase>:');
+		commitTitleParts.push('<conventional commits type>(<optional scope of the change>):');
 	}
 
 	if (useGitmoji) {
-		commitFormatParts.push('<gitmoji> <subject>)>:');
+		commitTitleParts.push('<gitmoji>');
 	}
 
-	if (!useGitmoji) {
-		commitFormatParts.push('<subject>)>:');
-	}
+	commitTitleParts.push('<commit title>');
 
-	commitFormatParts.push('<commit message>');
+	return commitTitleParts.join(' ');
+};
 
-	// <conventional commit type>: <gitmoji> <commit message>
-	return `The commit message should be in the following format: ${commitFormatParts.join(' ')}.`;
+const getCommitMessageFormatPrompt = (useConventionalCommits: boolean, useGitmoji: boolean) => {
+	const commitTitleFormat = getCommitTitleFormatPrompt(useConventionalCommits, useGitmoji);
+
+	return `The commit message should be in the following format:${commitTitleFormat}/n/n<commit description>`;
 };
 
 const getExtraContextForConventionalCommits = () => {
 	// Based on https://medium.com/neudesic-innovation/conventional-commits-a-better-way-78d6785c2e08
 	const conventionalCommitTypes: Record<string, string> = {
 		/*
-			Comment out feat: and fix: because they are too common and
+			Commented out feat: and fix: because they are too common and
 			will cause the model to generate them too often.
 		*/
 		// feat: 'The commit implements a new feature for the application.',
@@ -138,16 +145,15 @@ const getExtraContextForConventionalCommits = () => {
 		test: 'the commit changes the suite of automated tests for the product.',
 	};
 
-	const extraContextList = [];
-
+	let conventionalCommitDescription = '';
 	// eslint-disable-next-line guard-for-in
 	for (const key in conventionalCommitTypes) {
 		const value = conventionalCommitTypes[key];
-		const context = `When ${value} I want you to use the "${key}" conventional commit type.`;
-		extraContextList.push(context);
+
+		conventionalCommitDescription += `${key}: ${value}\n`;
 	}
 
-	return extraContextList.join(' ');
+	return `Choose a conventional commit type from the list below based on the git diff:\n${conventionalCommitDescription}`;
 };
 
 async function main() {
@@ -162,16 +168,6 @@ async function main() {
 const getExtraContextGitmoji = async () => {
 	const gitmojiDescriptions = await main();
 	return `Choose a gitmoji from the list below based on the conventional commit scope and git diff:\n${gitmojiDescriptions}`;
-};
-
-const gitCommitExample = (useConventionalCommits: boolean, useGitmoji: boolean) => {
-	const commitFormatParts = [];
-
-	if (useConventionalCommits && useGitmoji) {
-		commitFormatParts.push('EXAMPLE:\nfeat: 🎉 Initial commit\n\nThis commit is the first commit of the repository');
-	}
-
-	return `${commitFormatParts.join(' ')}.`;
 };
 
 const model = 'gpt-3.5-turbo';
@@ -191,10 +187,9 @@ export const generateCommitMessage = async (
 		useConventionalCommits, useGitmoji,
 	);
 
-	const exampleCommitWithConventionalGitmoji = gitCommitExample(
-		useConventionalCommits, useGitmoji,
-	);
+	const systemPrompt = `${basePrompt} ${commitMessageFormatPrompt}`;
 
+	const commitMessageExtraContext = getCommitMessageExtraContext(locale);
 	const conventionalCommitsExtraContext = useConventionalCommits ? getExtraContextForConventionalCommits() : '';
 
 	const gitMojiExtraContext = useGitmoji ? await getExtraContextGitmoji() : '';
@@ -203,26 +198,35 @@ export const generateCommitMessage = async (
 		return message.replace(/<br>/g, '\n').replace(/\n{3,}/g, '\n');
 	}
 
-	const prompt = `${basePrompt}${commitMessageFormatPrompt}\n${conventionalCommitsExtraContext}\n\n${gitMojiExtraContext}\n\n${exampleCommitWithConventionalGitmoji}\n\n`;
+	const assistantPrompt = `${commitMessageExtraContext}\n\n${conventionalCommitsExtraContext}\n\n${gitMojiExtraContext}`;
+
+	const allPromptContent = [systemPrompt, assistantPrompt, diff];
 
 	/**
 	 * text-davinci-003 has a token limit of 4000
 	 * https://platform.openai.com/docs/models/overview#:~:text=to%20Sep%202021-,text%2Ddavinci%2D003,-Can%20do%20any
 	 */
-	if (encoder.encode(prompt).length > 4000) {
+	if (encoder.encode(allPromptContent.join('')).length > 4000) {
 		throw new KnownError('The diff is too large for the OpenAI API. Try reducing the number of staged changes, or write your own commit message.');
 	}
 
 	try {
 		const completion = await createChatCompletion(apiKey, {
 			model,
-			messages: [{
-				role: 'system',
-				content: prompt,
-			}, {
-				role: 'user',
-				content: diff,
-			}],
+			messages: [
+				{
+					role: 'system',
+					content: systemPrompt,
+				},
+				{
+					role: 'assistant',
+					content: assistantPrompt,
+				},
+				{
+					role: 'user',
+					content: diff,
+				},
+			],
 			temperature: 0.7,
 			top_p: 1,
 			frequency_penalty: 0,
