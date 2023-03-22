e414cff0721dc776f24e8ec704496b83cb34934e
Thijs Limmen <tlimmen@xpirit.com>
2023-03-22 17:19:49 +0100

    refactor(tests): 🔧 Rename test files
    Rename the test files for better code organization and maintainability.

--- tests/index.ts
+++ tests/index.ts
@@ -1,6 +1,6 @@
 import { describe } from 'manten';
 
 describe('aicommits', ({ runTestSuite }) => {
-	runTestSuite(import('./specs/cli.js'));
-	runTestSuite(import('./specs/config.js'));
+	runTestSuite(import('./specs/cli.spec.js'));
+	runTestSuite(import('./specs/config.spec.js'));
 });
