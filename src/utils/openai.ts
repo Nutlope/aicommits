f5f2c6f03838190bde07c2ee739cb1668a77fdb8
Thijs Limmen <limmenthijs@gmail.com>
2023-03-23 16:19:19 +0100

    revert: openai usage through an HTTP post

--- src/utils/openai.ts
+++ src/utils/openai.ts
@@ -1,5 +1,88 @@
-import { ChatCompletionRequestMessage, Configuration, OpenAIApi } from 'openai';
+import https from 'https';
+import type { ClientRequest, IncomingMessage } from 'http';
+import type { ChatCompletionRequestMessage, CreateChatCompletionRequest, CreateChatCompletionResponse } from 'openai';
+import { encoding_for_model as encodingForModel } from '@dqbd/tiktoken';
 import { KnownError } from './error.js';
+
+const httpsPost = async (
+	hostname: string,
+	path: string,
+	headers: Record<string, string>,
+	json: unknown,
+) => new Promise<{
+	request: ClientRequest;
+	response: IncomingMessage;
+	data: string;
+}>((resolve, reject) => {
+	const postContent = JSON.stringify(json);
+	const request = https.request(
+		{
+			port: 443,
+			hostname,
+			path,
+			method: 'POST',
+			headers: {
+				...headers,
+				'Content-Type': 'application/json',
+				'Content-Length': Buffer.byteLength(postContent),
+			},
+			timeout: 10_000, // 10s
+		},
+		(response) => {
+			const body: Buffer[] = [];
+			response.on('data', chunk => body.push(chunk));
+			response.on('end', () => {
+				resolve({
+					request,
+					response,
+					data: Buffer.concat(body).toString(),
+				});
+			});
+		},
+	);
+	request.on('error', reject);
+	request.on('timeout', () => {
+		request.destroy();
+		reject(new KnownError('Request timed out'));
+	});
+
+	request.write(postContent);
+	request.end();
+});
+
+const createChatCompletion = async (
+	apiKey: string,
+	json: CreateChatCompletionRequest,
+) => {
+	const { response, data } = await httpsPost(
+		'api.openai.com',
+		'/v1/chat/completions',
+		{
+			Authorization: `Bearer ${apiKey}`,
+		},
+		json,
+	);
+
+	if (
+		!response.statusCode
+		|| response.statusCode < 200
+		|| response.statusCode > 299
+	) {
+		let errorMessage = `OpenAI API Error: ${response.statusCode} - ${response.statusMessage}`;
+
+		if (data) {
+			errorMessage += `\n\n${data}`;
+		}
+
+		if (response.statusCode === 500) {
+			errorMessage += '\n\nCheck the API status: https://status.openai.com';
+		}
+
+		throw new KnownError(errorMessage);
+	}
+
+	return JSON.parse(data) as CreateChatCompletionResponse;
+};
 
 const sanitizeMessage = (message: string) => message.trim().replace(/[\n\r]/g, '').replace(/(\w)\.$/, '$1');
 
@@ -55,6 +138,8 @@ const getExtraContextForConventionalCommits = () => {
 
 const model = 'gpt-3.5-turbo';
 
+const encoder = encodingForModel('text-davinci-003');
+
 export const generateCommitMessage = async (
 	apiKey: string,
 	locale: string,
@@ -87,45 +172,40 @@ export const generateCommitMessage = async (
 		},
 	];
 
-	const configuration = new Configuration({
-		apiKey,
-	});
+	const prompt = completionMessages.map(message => message.content).join('\n');
 
-	const openai = new OpenAIApi(configuration);
+	/**
+	 * text-davinci-003 has a token limit of 4000
+	 * https://platform.openai.com/docs/models/overview#:~:text=to%20Sep%202021-,text%2Ddavinci%2D003,-Can%20do%20any
+	 */
+	if (encoder.encode(prompt).length > 4000) {
+		throw new KnownError('The diff is too large for the OpenAI API. Try reducing the number of staged changes, or write your own commit message.');
+	}
 
 	try {
-		const completion = await openai.createChatCompletion({
+		const completion = await createChatCompletion(apiKey, {
 			model,
 			messages: completionMessages,
 			temperature: 0.7,
 			top_p: 1,
 			frequency_penalty: 0,
 			presence_penalty: 0,
+			max_tokens: 200,
 			stream: false,
 			n: completions,
 		});
 
 		return deduplicateMessages(
-			completion.data.choices
+			completion.choices
 				.filter(choice => choice.message?.content)
 				.map(choice => sanitizeMessage(choice.message!.content)),
 		);
-	} catch (error: any) {
-		if (error.code === 'ENOTFOUND') {
-			throw new KnownError(`Error connecting to ${error.hostname} (${error.syscall}). Are you connected to the internet?`);
+	} catch (error) {
+		const errorAsAny = error as any;
+		if (errorAsAny.code === 'ENOTFOUND') {
+			throw new KnownError(`Error connecting to ${errorAsAny.hostname} (${errorAsAny.syscall}). Are you connected to the internet?`);
 		}
 
-		const statusCode = error?.response?.status;
-		if (statusCode === 400) {
-			throw new KnownError(
-				'The diff is too large for the OpenAI API. Try reducing the number of staged changes, or write your own commit message.',
-			);
-		}
-
-		if (statusCode === 401) {
-			throw new KnownError('Unauthorized: The OPENAI_KEY that you configured is invalid.');
-		}
+		throw errorAsAny;
 	}
-
-	throw new Error('An unknown error occured, Please try again.');
 };
