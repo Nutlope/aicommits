a2cdcf56d6bd684a3d301c9b962450830e4d4879
Hiroki Osame <hiroki.osame@gmail.com>
2023-03-27 20:36:30 +0900

    test: git hook

---
+++ tests/specs/git-hook.ts
@@ -0,0 +1,34 @@
+import { testSuite, expect } from 'manten';
+import { createFixture, createGit, files } from '../utils.js';
+
+export default testSuite(({ describe }) => {
+	describe('Git hook', ({ test }) => {
+		test('errors when not in Git repo', async () => {
+			const { fixture, aicommits } = await createFixture(files);
+			const { exitCode, stderr } = await aicommits(['hook', 'install'], {
+				reject: false,
+			});
+
+			expect(exitCode).toBe(1);
+			expect(stderr).toMatch('The current directory must be a Git repository');
+
+			await fixture.rm();
+		});
+
+		test('Commits', async () => {
+			const { fixture, aicommits } = await createFixture(files);
+			const git = await createGit(fixture.path);
+
+			const { stdout } = await aicommits(['hook', 'install']);
+			expect(stdout).toMatch('Hook installed');
+
+			await git('add', ['data.json']);
+			await git('commit', ['--no-edit']);
+
+			const { stdout: commitMessage } = await git('log', ['--pretty=%B']);
+			console.log('Committed with:', commitMessage);
+
+			await fixture.rm();
+		});
+	});
+});
