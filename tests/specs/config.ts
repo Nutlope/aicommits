9038bd19db921355154ec7cbe29a96ad33da141c
Thijs Limmen <limmenthijs@gmail.com>
2023-03-27 12:07:04 +0200

    revert: revert changes to manten

--- tests/specs/config.ts
+++ tests/specs/config.ts
@@ -1,94 +1,54 @@
 import fs from 'fs/promises';
 import path from 'path';
-import { createFixture } from 'fs-fixture';
-import {
-	describe, it, beforeAll,
-} from 'vitest';
-import { createAicommits } from '../utils.js';
+import { testSuite, expect } from 'manten';
+import { createFixture } from '../utils.js';
 
-const { OPENAI_KEY } = process.env;
-
-describe('config', () => {
-	beforeAll(async () => {
-		if (!OPENAI_KEY) {
-			console.warn(
-				'⚠️  process.env.OPENAI_KEY is necessary to run these tests. Skipping...',
-			);
-		}
-	});
-
-	it.concurrent('set unknown config file', async ({ expect }) => {
-		const fixture = await createFixture();
-		const aicommits = createAicommits({
-			home: fixture.path,
-		});
-
-		const { stderr } = await aicommits(['config', 'set', 'UNKNOWN=1'], {
-			reject: false,
-		});
-
-		expect(stderr).toMatch('Invalid config property: UNKNOWN');
-	});
-
-	it.concurrent('set invalid OPENAI_KEY', async ({ expect }) => {
-		const fixture = await createFixture();
-		const aicommits = createAicommits({
-			home: fixture.path,
-		});
-
-		const { stderr } = await aicommits(['config', 'set', 'OPENAI_KEY=abc'], {
-			reject: false,
-		});
-
-		expect(stderr).toMatch(
-			'Invalid config property OPENAI_KEY: Must start with "sk-"',
-		);
-	});
-
-	it.concurrent('set config file', async ({ expect }) => {
-		const fixture = await createFixture();
-		const aicommits = createAicommits({
-			home: fixture.path,
-		});
+export default testSuite(({ describe }) => {
+	describe('config', async ({ test }) => {
+		const { fixture, aicommits } = await createFixture();
 		const configPath = path.join(fixture.path, '.aicommits');
+		const openAiToken = 'OPENAI_KEY=sk-abc';
 
-		const config = `OPENAI_KEY=${OPENAI_KEY}`;
-		await aicommits(['config', 'set', config]);
+		test('set unknown config file', async () => {
+			const { stderr } = await aicommits(['config', 'set', 'UNKNOWN=1'], {
+				reject: false,
+			});
 
-		const configFile = await fs.readFile(configPath, 'utf8');
-		expect(configFile).toMatch(config);
-	});
-
-	it.concurrent('get config file', async ({ expect }) => {
-		const fixture = await createFixture();
-		const aicommits = createAicommits({
-			home: fixture.path,
+			expect(stderr).toMatch('Invalid config property: UNKNOWN');
 		});
 
-		const config = `OPENAI_KEY=${OPENAI_KEY}`;
+		test('set invalid OPENAI_KEY', async () => {
+			const { stderr } = await aicommits(['config', 'set', 'OPENAI_KEY=abc'], {
+				reject: false,
+			});
 
-		await aicommits(['config', 'set', config]);
-		const { stdout } = await aicommits(['config', 'get', 'OPENAI_KEY']);
-		expect(stdout).toBe(config);
-	});
-
-	it.concurrent('reading unknown config', async ({ expect }) => {
-		const fixture = await createFixture();
-		const aicommits = createAicommits({
-			home: fixture.path,
-		});
-		const configPath = path.join(fixture.path, '.aicommits');
-
-		const config = `OPENAI_KEY=${OPENAI_KEY}`;
-		await aicommits(['config', 'set', config]);
-
-		await fs.appendFile(configPath, 'UNKNOWN=1');
-
-		const { stdout, stderr } = await aicommits(['config', 'get', 'UNKNOWN'], {
-			reject: false,
+			expect(stderr).toMatch('Invalid config property OPENAI_KEY: Must start with "sk-"');
 		});
 
-		expect(stdout).toBe('');
-		expect(stderr).toBe('');
+		await test('set config file', async () => {
+			await aicommits(['config', 'set', openAiToken]);
+
+			const configFile = await fs.readFile(configPath, 'utf8');
+			expect(configFile).toMatch(openAiToken);
+		});
+
+		await test('get config file', async () => {
+			const { stdout } = await aicommits(['config', 'get', 'OPENAI_KEY']);
+
+			expect(stdout).toBe(openAiToken);
+		});
+
+		await test('reading unknown config', async () => {
+			await fs.appendFile(configPath, 'UNKNOWN=1');
+
+			const { stdout, stderr } = await aicommits(['config', 'get', 'UNKNOWN'], {
+				reject: false,
+			});
+
+			expect(stdout).toBe('');
+			expect(stderr).toBe('');
+		});
+
+		await fixture.rm();
 	});
 });
