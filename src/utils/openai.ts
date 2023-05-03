c30e718bb9df9d03bebb4467d131ed6ad14df97f
Hiroki Osame <hiroki.osame@gmail.com>
2023-05-03 22:13:47 +0900

    lint

--- src/utils/openai.ts
+++ src/utils/openai.ts
@@ -125,29 +125,31 @@ const getCommitMessageFormat = (type: CommitType) => {
 	return '<commit message>';
 };
 
-const getExtraContextForConventionalCommits = () => {
-	/**
-	 * References:
-	 * Commitlint:
-	 * https://github.com/conventional-changelog/commitlint/blob/18fbed7ea86ac0ec9d5449b4979b762ec4305a92/%40commitlint/config-conventional/index.js#L40-L100
-	 *
-	 * Conventional Changelog:
-	 * https://github.com/conventional-changelog/conventional-changelog/blob/d0e5d5926c8addba74bc962553dd8bcfba90e228/packages/conventional-changelog-conventionalcommits/writer-opts.js#L182-L193
-	 */
-	return `Choose a type from the type-to-description JSON below that best describes the git diff:\n${JSON.stringify({
-		docs: 'Documentation only changes',
-		style: 'Changes that do not affect the meaning of the code (white-space, formatting, missing semi-colons, etc)',
-		refactor: 'A code change that neither fixes a bug nor adds a feature',
-		perf: 'A code change that improves performance',
-		test: 'Adding missing tests or correcting existing tests',
-		build: 'Changes that affect the build system or external dependencies',
-		ci: 'Changes to our CI configuration files and scripts',
-		chore: "Other changes that don't modify src or test files",
-		revert: 'Reverts a previous commit',
-		feat: 'A new feature',
-		fix: 'A bug fix',
-	}, null, 2)}`;
-};
+/**
+ * References:
+ * Commitlint:
+ * https://github.com/conventional-changelog/commitlint/blob/18fbed7ea86ac0ec9d5449b4979b762ec4305a92/%40commitlint/config-conventional/index.js#L40-L100
+ *
+ * Conventional Changelog:
+ * https://github.com/conventional-changelog/conventional-changelog/blob/d0e5d5926c8addba74bc962553dd8bcfba90e228/packages/conventional-changelog-conventionalcommits/writer-opts.js#L182-L193
+ */
+const getExtraContextForConventionalCommits = () => (
+	`Choose a type from the type-to-description JSON below that best describes the git diff:\n${
+		JSON.stringify({
+			docs: 'Documentation only changes',
+			style: 'Changes that do not affect the meaning of the code (white-space, formatting, missing semi-colons, etc)',
+			refactor: 'A code change that neither fixes a bug nor adds a feature',
+			perf: 'A code change that improves performance',
+			test: 'Adding missing tests or correcting existing tests',
+			build: 'Changes that affect the build system or external dependencies',
+			ci: 'Changes to our CI configuration files and scripts',
+			chore: "Other changes that don't modify src or test files",
+			revert: 'Reverts a previous commit',
+			feat: 'A new feature',
+			fix: 'A bug fix',
+		}, null, 2)
+	}`
+);
 
 const generateStringFromLength = (length: number) => {
 	let result = '';
