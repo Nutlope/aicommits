31ddfb9e76c2d1b4d9b56db9187b6eb365ee82a8
johnhuitsai <caiqizhe@gmail.com>
2023-09-23 14:15:31 +0800

    fix: remove parseAssert OPENAI_KEY

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -28,8 +28,6 @@ const configParsers = {
 		if (!key) {
 			throw new KnownError('Please set your OpenAI API key via `aicommits config set OPENAI_KEY=<your token>`');
 		}
-		parseAssert('OPENAI_KEY', key.startsWith('sk-') || key.startsWith('pk-'), 'Must start with "[sk|pk]-"');
-		// Key can range from 43~51 characters. There's no spec to assert this.
 
 		return key;
 	},
