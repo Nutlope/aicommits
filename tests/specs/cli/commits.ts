1b52d5c9bfd24d48fe654584b7cb61849d292bbe
Elminson De Oleo Baez <edeoleo@gmail.com>
2023-09-30 01:35:51 -0400

    Fix commit confirmation issue in CLI commits test

--- tests/specs/cli/commits.ts
+++ tests/specs/cli/commits.ts
@@ -388,8 +388,8 @@ export default testSuite(({ describe }) => {
 					const stdout = buffer.toString();
 					if (stdout.match('└')) {
 						committing.stdin!.write('y');
-						committing.stdin!.end();
 					}
+					committing.stdin!.end();
 				});
 
 				await committing;
