66c295407e14ed9a1f16f6d905cf20e463ff4db2
Elminson De Oleo Baez <edeoleo@gmail.com>
2023-09-30 01:35:04 -0400

    Refactor code to improve readability and remove unnecessary lines

--- tests/specs/cli/commits.ts
+++ tests/specs/cli/commits.ts
@@ -257,8 +257,8 @@ export default testSuite(({ describe }) => {
 					const stdout = buffer.toString();
 					if (stdout.match('└')) {
 						committing.stdin!.write('y');
-						committing.stdin!.end();
 					}
+					committing.stdin!.end();
 				});
 
 				await committing;
