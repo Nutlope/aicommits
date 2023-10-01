f556393b921d8c4d82bbed860ab255f63c4486cb
蔡俊辉 <caiqizhe@gmail.com>
2023-10-01 08:52:50 +0800

    support socks

--- src/utils/openai.ts
+++ src/utils/openai.ts
@@ -2,104 +2,105 @@ import https from 'https';
 import type { ClientRequest, IncomingMessage } from 'http';
 import type { CreateChatCompletionRequest, CreateChatCompletionResponse } from 'openai';
 import {
-	type TiktokenModel,
-	// encoding_for_model,
+  type TiktokenModel,
+  // encoding_for_model,
 } from '@dqbd/tiktoken';
 import createHttpsProxyAgent from 'https-proxy-agent';
+import { SocksProxyAgent } from 'socks-proxy-agent';
 import { KnownError } from './error.js';
 import type { CommitType } from './config.js';
 import { generatePrompt } from './prompt.js';
 
 const httpsPost = async (
-	hostname: string,
-	path: string,
-	headers: Record<string, string>,
-	json: unknown,
-	timeout: number,
-	proxy?: string,
+  hostname: string,
+  path: string,
+  headers: Record<string, string>,
+  json: unknown,
+  timeout: number,
+  proxy?: string,
 ) => new Promise<{
-	request: ClientRequest;
-	response: IncomingMessage;
-	data: string;
+  request: ClientRequest;
+  response: IncomingMessage;
+  data: string;
 }>((resolve, reject) => {
-	const postContent = JSON.stringify(json);
-	const request = https.request(
-		{
-			port: 443,
-			hostname,
-			path,
-			method: 'POST',
-			headers: {
-				...headers,
-				'Content-Type': 'application/json',
-				'Content-Length': Buffer.byteLength(postContent),
-			},
-			timeout,
-			agent: (
-				proxy
-					? createHttpsProxyAgent(proxy)
-					: undefined
-			),
-		},
-		(response) => {
-			const body: Buffer[] = [];
-			response.on('data', chunk => body.push(chunk));
-			response.on('end', () => {
-				resolve({
-					request,
-					response,
-					data: Buffer.concat(body).toString(),
-				});
-			});
-		},
-	);
-	request.on('error', reject);
-	request.on('timeout', () => {
-		request.destroy();
-		reject(new KnownError(`Time out error: request took over ${timeout}ms. Try increasing the \`timeout\` config, or checking the OpenAI API status https://status.openai.com`));
-	});
+  const postContent = JSON.stringify(json);
+  const request = https.request(
+    {
+      port: 443,
+      hostname,
+      path,
+      method: 'POST',
+      headers: {
+        ...headers,
+        'Content-Type': 'application/json',
+        'Content-Length': Buffer.byteLength(postContent),
+      },
+      timeout,
+      agent: (
+        proxy
+          ? (/^https?:\/\//.test(proxy) ? createHttpsProxyAgent(proxy) : new SocksProxyAgent(proxy))
+          : undefined
+      ),
+    },
+    (response) => {
+      const body: Buffer[] = [];
+      response.on('data', chunk => body.push(chunk));
+      response.on('end', () => {
+        resolve({
+          request,
+          response,
+          data: Buffer.concat(body).toString(),
+        });
+      });
+    },
+  );
+  request.on('error', reject);
+  request.on('timeout', () => {
+    request.destroy();
+    reject(new KnownError(`Time out error: request took over ${timeout}ms. Try increasing the \`timeout\` config, or checking the OpenAI API status https://status.openai.com`));
+  });
 
-	request.write(postContent);
-	request.end();
+  request.write(postContent);
+  request.end();
 });
 
 const createChatCompletion = async (
-	apiKey: string,
-	json: CreateChatCompletionRequest,
-	timeout: number,
-	proxy?: string,
-	hostname = 'api.openai.com',
+  apiKey: string,
+  json: CreateChatCompletionRequest,
+  timeout: number,
+  proxy?: string,
+  hostname = 'api.openai.com',
 ) => {
-	const { response, data } = await httpsPost(
-		hostname,
-		'/v1/chat/completions',
-		{
-			Authorization: `Bearer ${apiKey}`,
-		},
-		json,
-		timeout,
-		proxy,
-	);
+  const { response, data } = await httpsPost(
+    hostname,
+    '/v1/chat/completions',
+    {
+      Authorization: `Bearer ${apiKey}`,
+    },
+    json,
+    timeout,
+    proxy,
+  );
 
-	if (
-		!response.statusCode
-		|| response.statusCode < 200
-		|| response.statusCode > 299
-	) {
-		let errorMessage = `OpenAI API Error: ${response.statusCode} - ${response.statusMessage}`;
+  if (
+    !response.statusCode
+    || response.statusCode < 200
+    || response.statusCode > 299
+  ) {
+    let errorMessage = `OpenAI API Error: ${response.statusCode} - ${response.statusMessage}`;
 
-		if (data) {
-			errorMessage += `\n\n${data}`;
-		}
+    if (data) {
+      errorMessage += `\n\n${data}`;
+    }
 
-		if (response.statusCode === 500) {
-			errorMessage += '\n\nCheck the API status: https://status.openai.com';
-		}
+    if (response.statusCode === 500) {
+      errorMessage += '\n\nCheck the API status: https://status.openai.com';
+    }
 
-		throw new KnownError(errorMessage);
-	}
+    throw new KnownError(errorMessage);
+  }
 
-	return JSON.parse(data) as CreateChatCompletionResponse;
+  return JSON.parse(data) as CreateChatCompletionResponse;
 };
 
 const sanitizeMessage = (message: string) => message.trim().replace(/[\n\r]/g, '').replace(/(\w)\.$/, '$1');
@@ -124,56 +125,56 @@ const deduplicateMessages = (array: string[]) => Array.from(new Set(array));
 // };
 
 export const generateCommitMessage = async (
-	apiKey: string,
-	model: TiktokenModel,
-	locale: string,
-	diff: string,
-	completions: number,
-	maxLength: number,
-	type: CommitType,
-	timeout: number,
-	proxy?: string,
-	baseUrl?: string,
+  apiKey: string,
+  model: TiktokenModel,
+  locale: string,
+  diff: string,
+  completions: number,
+  maxLength: number,
+  type: CommitType,
+  timeout: number,
+  proxy?: string,
+  baseUrl?: string,
 ) => {
-	try {
-		const completion = await createChatCompletion(
-			apiKey,
-			{
-				model,
-				messages: [
-					{
-						role: 'system',
-						content: generatePrompt(locale, maxLength, type),
-					},
-					{
-						role: 'user',
-						content: diff,
-					},
-				],
-				temperature: 0.7,
-				top_p: 1,
-				frequency_penalty: 0,
-				presence_penalty: 0,
-				max_tokens: 200,
-				stream: false,
-				n: completions,
-			},
-			timeout,
-			proxy,
-			baseUrl,
-		);
+  try {
+    const completion = await createChatCompletion(
+      apiKey,
+      {
+        model,
+        messages: [
+          {
+            role: 'system',
+            content: generatePrompt(locale, maxLength, type),
+          },
+          {
+            role: 'user',
+            content: diff,
+          },
+        ],
+        temperature: 0.7,
+        top_p: 1,
+        frequency_penalty: 0,
+        presence_penalty: 0,
+        max_tokens: 200,
+        stream: false,
+        n: completions,
+      },
+      timeout,
+      proxy,
+      baseUrl,
+    );
 
-		return deduplicateMessages(
-			completion.choices
-				.filter(choice => choice.message?.content)
-				.map(choice => sanitizeMessage(choice.message!.content)),
-		);
-	} catch (error) {
-		const errorAsAny = error as any;
-		if (errorAsAny.code === 'ENOTFOUND') {
-			throw new KnownError(`Error connecting to ${errorAsAny.hostname} (${errorAsAny.syscall}). Are you connected to the internet?`);
-		}
+    return deduplicateMessages(
+      completion.choices
+        .filter(choice => choice.message?.content)
+        .map(choice => sanitizeMessage(choice.message!.content)),
+    );
+  } catch (error) {
+    const errorAsAny = error as any;
+    if (errorAsAny.code === 'ENOTFOUND') {
+      throw new KnownError(`Error connecting to ${errorAsAny.hostname} (${errorAsAny.syscall}). Are you connected to the internet?`);
+    }
 
-		throw errorAsAny;
-	}
+    throw errorAsAny;
+  }
 };
