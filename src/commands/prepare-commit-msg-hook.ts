0ce4f9e04b927e12e05de4b3918cec5953d3d7b4
Naftoli Gugenheim <98384+nafg@users.noreply.github.com>
2024-02-13 19:28:15 -0500

    fix: prepend instructions to commit message

--- src/commands/prepare-commit-msg-hook.ts
+++ src/commands/prepare-commit-msg-hook.ts
@@ -87,7 +87,10 @@ export default () =>
 			instructions += `\n${messages[0]}\n`;
 		}
 
-		await fs.appendFile(messageFilePath, instructions);
+		const currentContent = await fs.readFile(messageFilePath, 'utf8');
+		const newContent = instructions + '\n' + currentContent;
+		await fs.writeFile(messageFilePath, newContent);
+
 		outro(`${green('✔')} Saved commit message!`);
 	})().catch((error) => {
 		outro(`${red('✖')} ${error.message}`);
